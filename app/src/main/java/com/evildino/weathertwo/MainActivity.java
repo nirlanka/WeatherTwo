8083dd8a9d9c21b9c977dfe0c61737a9db13ee15
Nirmal Lanka <nir3al@gmail.com>
2019-03-26 19:05:40 +0530

    Set activity background color

--- app/src/main/java/com/evildino/weathertwo/MainActivity.java
+++ app/src/main/java/com/evildino/weathertwo/MainActivity.java
@@ -2,6 +2,7 @@ package com.evildino.weathertwo;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -13,5 +14,7 @@ public class MainActivity extends Activity {
 
         ActionBar actionBar = getActionBar();
         actionBar.hide();
+
+        getWindow().getDecorView().setBackgroundColor(Color.parseColor("#293251"));
     }
 }
